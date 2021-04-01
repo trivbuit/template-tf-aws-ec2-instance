8a2805d0e444eb2409197641a7a630914ae459fc
Tri Van BUI <trivbuit@gmail.com>
2021-04-01 16:26:07 +0700

    Fix count

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "ec2-instance-trmodule" {
   source  = "trivbuit.scalr.io/env-tct37d1oddo7u4g/ec2-instance-trmodule/aws"
   version = "1.0.4"
 
-  count = 1
+  count = 2
 
   ami           = "ami-01581ffba3821cdf3"
   instance_type = "t2.micro"
