007cd1ae95367f12b14a8c099b17419a9fe0efdb
Tri Van BUI <trivbuit@gmail.com>
2021-04-01 17:41:52 +0700

    Fix key & count

--- main.tf
+++ main.tf
@@ -7,15 +7,15 @@ variable "instance_type" {
   type = string
 }
 
-terraform {
-  backend "s3" {
-    bucket     = "rt-tf-test-bucket"
-    key        = "terraform.tfstate"
-    region     = "ap-southeast-1"
-    access_key = "AKIA32GTTIRLA5S2MNE5"
-    secret_key = "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"
-  }
-}
+// terraform {
+//   backend "s3" {
+//     bucket     = "rt-tf-test-bucket"
+//     key        = "terraform.tfstate"
+//     region     = "ap-southeast-1"
+//     access_key = "access_key"
+//     secret_key = "secret_key"
+//   }
+// }
 
 # Configure the AWS Provider
 provider "aws" {
@@ -26,7 +26,7 @@ module "ec2-instance-trmodule" {
   source  = "trivbuit.scalr.io/env-tct37d1oddo7u4g/ec2-instance-trmodule/aws"
   version = "1.0.4"
 
-  count = 2
+  count = 1
 
   ami           = "ami-01581ffba3821cdf3"
   instance_type = var.instance_type
