f2374fc36d00e129c92ccd7f711a181852be034d
Tri Van BUI <trivbuit@gmail.com>
2021-04-01 16:17:50 +0700

    Update new template

---
+++ main.tf
@@ -0,0 +1,31 @@
+
+terraform {
+  backend "s3" {
+    bucket     = "rt-tf-test-bucket"
+    key        = "terraform.tfstate"
+    region     = "ap-southeast-1"
+    access_key = "AKIA32GTTIRLA5S2MNE5"
+    secret_key = "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"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+module "ec2-instance-trmodule" {
+  source  = "trivbuit.scalr.io/env-tct37d1oddo7u4g/ec2-instance-trmodule/aws"
+  version = "1.0.4"
+
+  count = 1
+
+  ami           = "ami-01581ffba3821cdf3"
+  instance_type = "t2.micro"
+
+  key_name = "trbvm"
+}
+
+output "ec2-user" {
+  value = module.ec2-instance-trmodule.*.ec2_user
+}
